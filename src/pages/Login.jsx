fc9c7ed8b4f06880792fefa9e03d0e9ba451a169
ranepriyanshu <priyanshurane09@gmail.com>
2024-12-07 23:19:22 +0530

    form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import { useState } from 'react';
+import {Container, Row, Col, Form, Button} from 'react-bootstrap'
 
 const Login = () => {
+  const [validated, setValidated] = useState(false);
+
+  //function to handle form submission 
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
-    <div>Login</div>
+    <div className="login-section align-content-center ">
+      <Container>
+        <Row className='justify-content-center'>
+          <Col xl={4} lg={5} md={7} xs={12}>
+          <div className="login-box  rounded p-4 shadow-sm bg-light">
+            <h3 className='md-4'>Sign In</h3>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+
+          </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
