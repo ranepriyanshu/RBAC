27b88c9fbad0eb31364078b425af9ff018af6d2b
ranepriyanshu <priyanshurane09@gmail.com>
2024-12-07 23:21:02 +0530

    form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,9 +27,9 @@ const Login = () => {
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+        <Form.Control.Feedback type="invalid">
+            Please provide a valid city.
+          </Form.Control.Feedback>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
