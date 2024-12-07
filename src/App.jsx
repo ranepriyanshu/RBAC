3337e8ae8c7d12c6e80494d753b9c2774c2b6812
ranepriyanshu <priyanshurane09@gmail.com>
2024-12-07 22:34:16 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Header } from './components/Header'
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import AdminPanel from './pages/AdminPanel'
+import { Users } from './pages/Users'
 
-const app = () => {
+
+const App = () => {
   return (
-    <div>app</div>
+  <>
+  <Header/>
+  <Routes>
+    <Route path='/' element={<Login/>}></Route>
+    <Route path='dashboard' element={<Dashboard/>}></Route>
+    <Route path='user-info' element={<Users/>}></Route>
+    <Route path='admin' element={<AdminPanel/>}></Route>
+  </Routes>
+  </>
   )
 }
 
-export default app
+export default App
