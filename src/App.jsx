4a97c4d820f942f41300285be87a19b28f101690
ranepriyanshu <priyanshurane09@gmail.com>
2024-12-07 22:59:21 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
-import { Header } from './components/Header'
+
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import AdminPanel from './pages/AdminPanel'
 import { Users } from './pages/Users'
+import Header from './components/Header'
+import UserInfo from './pages/UserInfo'
 
 
 const App = () => {
   return (
   <>
-  <Header/>
+  <Header></Header>
   <Routes>
     <Route path='/' element={<Login/>}></Route>
     <Route path='dashboard' element={<Dashboard/>}></Route>
-    <Route path='user-info' element={<Users/>}></Route>
+    <Route path='user-info' element={<UserInfo/>}></Route>
+    <Route path='user' element={<Users/>}></Route>
     <Route path='admin' element={<AdminPanel/>}></Route>
   </Routes>
   </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,27 @@
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/esm/Button';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from 'react-router-dom';
+
+
 
 function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">PR's Portal</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="ms-auto">
+            <NavLink className="ms-2 nav-link" to ="dashboard" >Home</NavLink>
+            <NavLink className="ms-2 nav-link" to="admin">AdminPanel</NavLink>
+            <NavLink className="ms-2 nav-link" to="user">Users</NavLink>
+            <NavLink className="ms-2 nav-link" to="user-info">User Info</NavLink>
+           
+          <Button varient = "primary" className='ms-3'>
+            <NavLink className="text-white" to='/'>Login</NavLink>
+          </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -32,3 +30,4 @@ function Header() {
 }
 
 export default Header;
+
