62554242310ba6457c808e2908534d7f44e4845a
ranepriyanshu <priyanshurane09@gmail.com>
2024-12-08 00:35:11 +0530

    form submission

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,13 +3,47 @@ import { useState } from 'react';
 import {Container, Row, Col, Form, Button} from 'react-bootstrap'
 
 const Login = () => {
+
+  const [showPassword, setShowPassword] = useState(false);
+  
+  const [isLoading, setLoading] = useState(false);
+
+  
+
   const [validated, setValidated] = useState(false);
+
+  //state to hold form data
+
+  const [formData , setFormData] = useState({
+
+    email: "",
+    password: "",
+  });
+
+
+  //function to handle change in form fields
+
+  const handleChange=(event)=>{
+
+    const{name, value}=event.target;
+
+    //update form data state with new values 
+
+    setFormData((prev)=>({
+
+        ...prev,
+        [name]: value,
+    })
+  )
+  }
 
   //function to handle form submission 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
+    event.preventDefault();
+
     if (form.checkValidity() === false) {
-      event.preventDefault();
+      
       event.stopPropagation();
     }
 
@@ -26,19 +60,40 @@ const Login = () => {
           <Form noValidate validated={validated} onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email"
+        value={formData.email}
+        onChange={handleChange}
+        name = "email"
+         placeholder="Enter email"
+         required
+         />
         <Form.Control.Feedback type="invalid">
-            Please provide a valid city.
+            Please provide a valid email address.
           </Form.Control.Feedback>
       </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      
+      <Form.Group className="mb-3 position-relative " controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type={showPassword ? 'text' : 'password'}
+        value={formData.password}
+        onChange={handleChange}
+        name = "password"
+        placeholder="Password" 
+        required
+        />
+        
+        <Form.Control.Feedback type="invalid">
+            Please provide a valid Password.
+          </Form.Control.Feedback>
+    
+        <span className=' position-absolute top-50 end-0  me-2 ' onClick={()=>
+          setShowPassword(!showPassword)
+        }>
+
+          {showPassword ? "hide" : "show"}
+        </span>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
+      
       <Button variant="primary" type="submit">
         Submit
       </Button>
